9be6f7fb4fba6e1dd6cf400d5ec3457a6dc6ff5a
shahdurran <shahdurranaq@gmail.com>
2022-04-04 06:01:33 +0500

    start of task 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Task1 from './components/task1'
 import Task2 from './components/task2'
+import Task3 from './components/task3'
 function App() {
   return (
     <div className="App">
       <Task1/>
       <Task2/>
+      <Task3/>
     </div>
   );
 }
---
+++ src/components/task3.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Task3 = () => {
+  return (
+    <div>Task 3</div>
+  )
+}
+export default Task3;
